264172f69ca60d406175b1f0b404ae89cc3f8df3
nahyan0077 <nahyanmp4@gmail.com>
2024-09-06 17:48:39 +0530

    feat: added following

---
+++ frontend/src/components/following/FollowingList.tsx
@@ -0,0 +1,44 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { endpoints } from '../../config/apiEndpoints'
+import { useNavigate, useParams } from 'react-router-dom'
+import './FollowersList.css'
+
+export const FollowingList:  React.FC = () => {
+    const params = useParams()
+    console.log(`${endpoints.gitHubData}/${params.userName}/followers`);
+    const [followers, setFollower] = useState([])
+    const navigate = useNavigate()
+    
+    useEffect(()=>{
+        async function fetchFollowers() {
+            const response = await axios.get(`${endpoints.gitHubData}${params.userName}/followers`)
+            console.log("followers",response);
+            setFollower(response.data) 
+        }
+        fetchFollowers()
+    },[])
+
+  return (
+    <>
+    <div className='followers-header' >
+
+        <h2> Followers </h2>
+    </div>
+        <div className='followers' >
+            {
+            followers.map((data: any, index) =>{
+                return (
+
+                <div className='followers-list' onClick={()=>navigate(`/follower/${data.login}`)} key={index} >
+                    <img src={data.avatar_url} alt="" />
+                    <h6> {data.login} </h6>
+                </div>
+                )
+            })
+            }
+        </div> 
+    </>
+  )
+}
+
