0c8eb43a839c4bee928da715ab734cfe8b39df4b
nahyan0077 <nahyanmp4@gmail.com>
2024-09-07 08:08:33 +0530

    fix: typos

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -16,6 +16,7 @@ export const Home: React.FC = () => {
       {
         Object.keys(githubData.userDetails).length ? (
           <>
+          
           <Profile />
           <Repositories />
           </>
